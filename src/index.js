3c44a5f6fd4f00aabca15ca78c5123a9255d5663
Alejae1998 <alejagr1998@gmail.com>
2023-06-30 10:32:16 -0700

    fetching api

--- src/index.js
+++ src/index.js
@@ -3,3 +3,31 @@ require('dotenv').config();
 
 const API_KEY = process.env.API_KEY;
 const form = document.getElementById('currencyForm');
+const results = document.getElementById('result');
+
+async function convertCurrency(amount, currency) {
+  try {
+    const response = await fetch(
+      `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`
+    );
+    const data = await response.json();
+
+    if (response.ok) {
+      const rate = data.coversion_rates[currency];
+
+      if (rate) {
+        const convertedAmount = amount * rate;
+        results.textContent = `${amount} USD = ${convertedAmount.toFixed(
+          2
+        )} ${currency}`;
+      } else {
+        results.textContent = `Currency "${currency}" not found.`;
+      }
+    } else {
+      results.textContent = `Error: ${data.error_type}. ${data.error_message}`;
+    }
+  } catch (error) {
+    results.textContent = 'An error ocurred while fetching the rates';
+    console.error(error);
+  }
+}
